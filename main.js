7a55229ae5475f11ebd2db1d0d4aa56582049f67
Francisco-Chung <fr.chung90@gmail.com>
2023-09-12 01:46:52 -0300

    first commit

--- main.js
+++ main.js
@@ -8,38 +8,27 @@ const fetchPokemon = async (number) => {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
         const data = await response.json();
-        
-        const name = data.name
-        const types = data.types.map(type => type.type.name).join(' / ')
-        const height = (data.height / 10).toFixed(1);
-        const weight = (data.weight / 10).toFixed(1);
-        const img = data.sprites.other["official-artwork"].front_default;
-        
-        const createPokemonTemplate = (name, img, types, height, weight) => {
-            return `
+
+        container.innerHTML =
+        `
             <div class="description">
-            <img src="${img}" alt="${name}">
-            <h3 class="name">${name}</h3>
-            <p class="type">Tipo: ${types}</p>
-            <p class="height">Altura: ${height} m</span>
-            <p class="weight">Peso: ${weight} kg</span>
+                <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
+                <h3 class="name">${data.name}</h3>
+                <p>Número: ${data.id}
+                <p>Tipo: ${data.types.map(type => type.type.name).join(' / ')}</p>
+                <p>Altura: ${(data.height / 10).toFixed(1)}m</span>
+                <p>Peso: ${(data.weight / 10).toFixed(1)}kg</span>
             </div>
-            `
-        }
-        const pokemonTemplate = createPokemonTemplate(name, img, types, height, weight)
-        container.innerHTML = pokemonTemplate
+        `;
+
     } catch (error) {
-        showError("No se ha encontrado el pokémon")
+        showError(`No se ha encontrado el pokémon con el número ${input.value}`)
     }
 };
 
 
 const showError = (msg) => {
-    const errorMsg = document.createElement("div")
-    errorMsg.classList.add("error")
-    errorMsg.textContent = msg
-    container.innerHTML = "";
-    container.appendChild(errorMsg)
+    container.innerHTML = `<p class="error">${msg}</p>`
 };
 
 
@@ -47,10 +36,11 @@ const searchPokemon = async (e) => {
     e.preventDefault();
     const pokemonId = input.value
     if (!pokemonId) {
-        showError('Ingrese un número válido')
+        showError('Ingrese un número para buscar el pokémon deseado')
         return;
     }
     await fetchPokemon(pokemonId);
+    form.reset();
 };
 
 
